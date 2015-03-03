9a35b3276512df3b149aa8091eac79063dcf5630
Actimia <actimia1@gmail.com>
2015-03-03 22:05:14 +0100

    Share commit

--- source/app.d
+++ source/app.d
@@ -1,18 +1,21 @@
 import vibe.d;
 
-shared static this()
-{
+shared static this(){
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 
 	auto router = new URLRouter;
-	router.get("/", &hello);
+	router.get("/", &index);
+	router.get("*", serveStaticFiles("./public/"));
 
 	listenHTTP(settings, router);
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
+void index(HTTPServerRequest req, HTTPServerResponse res){
 	res.render!("index.dt", res);
+}
+
+void error(HTTPServerRequest req, HTTPServerResponse res){
+	res.render!("error.dt", res);
 }
